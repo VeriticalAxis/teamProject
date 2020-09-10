cdfbd471314613ca15bfc04deb9389e7345a30c5
Arthur Zhang <49824241+VeriticalAxis@users.noreply.github.com>
2020-09-10 17:54:58 +0800

    my first commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+from manimlib.imports import *
+
+class HelloWorld(Scene):
+    def construct(self):
+        helloWorld = TextMobject("Hello world!")
+        self.play(Write(helloWorld))
+        self.wait()
